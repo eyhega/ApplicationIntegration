b62886eb1bcbad286a5fa26ad9ae814f637f8d2b
Gaetan Eyheramono <eyheramo@a212pc001.rcisima.isima.fr>
2013-11-05 16:32:56 +0100

    TP2 de la matière nommée "Intégration d'applications"

---
+++ EJB_ED/src/java/tp2/DAO/IDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2.DAO;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author eyheramo
+ */
+@Stateless(mappedName="Interface")
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class IDao implements IDaoLocal {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    //ajouter les méthodes
+    
+}
---
+++ EJB_ED/src/java/tp2/DAO/IDaoLocal.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2.DAO;
+
+/**
+ *
+ * @author eyheramo
+ */
+interface IDaoLocal {
+    
+}
